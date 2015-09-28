80ab9cc9b2923f9468e06e45512abd66fddaf447
Michael McClimon <michael@mcclimon.org>
2015-09-28 14:05:08 -0400

    Fix minor bugs.
    Don't attempt to highlight if there are no abstracts, and don't make
    visited anchor links turn dark blue.

--- js/session.js
+++ js/session.js
@@ -5,12 +5,17 @@ Zepto(function($) {
         num = titles.length,
         i, genId;
 
+    // link room number
+    $('span.room').first().wrapInner('<a href="../../../maps/"></a>');
+
+    // don't try to link if there are no abstracts
+    if (abstractTitles.length === 0) {
+        return;
+    }
+
     for (i = 0; i < num; i++) {
         genId = 'abs_' + i;
         abstractTitles[i].id = genId
         $(titles[i]).wrapInner('<a href="#'+ genId + '"></a>');
     }
-
-    $('span.room').first().wrapInner('<a href="../../../maps/"></a>');
-
 });
