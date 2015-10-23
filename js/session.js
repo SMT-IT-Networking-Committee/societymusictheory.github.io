a9f0d27e33e45c514bb0e7f528cff88199eea557
Michael McClimon <michael@mcclimon.org>
2015-10-23 09:12:51 -0400

    Preliminary work on handout linking.

--- js/session.js
+++ js/session.js
@@ -1,3 +1,5 @@
+var SMT = {};
+
 Zepto(function($) {
 
     var titles = $('p.title'),
@@ -15,7 +17,26 @@ Zepto(function($) {
 
     for (i = 0; i < num; i++) {
         genId = 'abs_' + i;
+        if (!abstractTitles[i]) { continue; }
         abstractTitles[i].id = genId
         $(titles[i]).wrapInner('<a href="#'+ genId + '"></a>');
     }
+
+    SMT.linkHandouts($);
 });
+
+
+SMT.linkHandouts = function($) {
+    // Get author last name as lowercase.
+    // NB: This only gets the first author's name, and doesn't work if there
+    // are non-ASCII characters. There are few enough of those that we can
+    // special-case them manually.
+    $('p.author').each(function(_, elem) {
+        var matches = $(elem).text().match(/\s([A-Za-z\-]+)\s+\(/);
+        if (matches !== null && matches[1]) {
+            var lcName = matches[1].toLowerCase();
+
+            // TODO: generate JSON list of handouts, wire up the links here
+        }
+    });
+};
