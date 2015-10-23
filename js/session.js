7e0a90ea8aa3021f66c0246041e8fcb768b7023a
Michael McClimon <michael@mcclimon.org>
2015-10-23 12:09:36 -0400

    Fix typo in link locations.

--- js/session.js
+++ js/session.js
@@ -60,7 +60,7 @@ SMT.linkHandouts = function($) {
         // if key isn't in the hash, give up
         if (!filename) { return; }
 
-        href = '/files/2015_handouts/' + filename;
+        href = '//societymusictheory.org/files/2015_handouts/' + filename;
         $(elem).append('&emsp;<a href="'+href+'"><i class="fa fa-file"></i></i>');
     });
 };
