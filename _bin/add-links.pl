21c957506eeff02b46d9374654a6cd85d402dab2
Michael McClimon <michael@mcclimon.org>
2018-10-19 11:23:04 -0400

    Update link-adder for new year

--- _bin/add-links.pl
+++ _bin/add-links.pl
@@ -5,64 +5,42 @@ use Path::Tiny qw(path);
 use YAML ();
 use Data::Dumper::Concise;
 use experimental qw(signatures);
+use utf8;
 
 # Read YAML sessions
 
-my $session_dir = path('sessions');
+my %days = (
+  Thursday => 'thu',
+  Friday   => 'fri',
+  Saturday => 'sat',
+  Sunday   => 'sun',
+);
+
+sub compute_time ($s) {
+  my @words = split /[ ,]/, $s;
+  my $out = "" . $days{ $words[0] } . "/" . $words[1];
+  return $out;
+}
+
+my $session_dir = path('_data/sessions');
 my %papers;
 
 for my $f ($session_dir->children) {
   next unless $f =~ /yml$/;
 
-  my $yml = YAML::LoadFile( "$f" );
+  my $slug;
 
-  my $time = compute_time($yml->{time});
-  my $link = "/sessions/$time/$yml->{slug}.html";
+  $f->edit_lines(sub {
+    my $l = $_;
+    chomp $l;
 
-  $papers{$_} = $link for $yml->{papers}->@*;
+    if ($l =~ /^slug:/) {
+      ($slug) = $l =~ /^slug:\s*(.*)/;
+    }
+
+    if (my ($time) = $l =~ /^time: ['"](.*)['"]/) {
+      my $link = compute_time($time);
+      $_ = "$l\nlink: /sessions/$link/$slug\n";
+    }
+  });
 }
-
-$papers{$_} = '/sessions/sat/afternoon' for qw(horlacher rings tenzer);
-
-# Do the papers
-
-my $papers_dir = path('papers');
-
-for my $f ($papers_dir->children) {
-  next unless $f =~ /yml$/;
-
-  my $key = $f->basename('.yml');
-  my $link = $papers{$key};
-  warn "No link for $key\n" and next unless $link;
-
-  my @lines = $f->lines_utf8;
-
-  my $out = Path::Tiny->tempfile;
-
-  for my $l (@lines) {
-    $out->append_utf8($l);
-    $out->append(qq{link: "$link"\n}) if $l =~ /^title/;
-  }
-
-  $out->move("$f");
-}
-
-
-
-my %days;
-
-BEGIN {
-  %days = (
-    Thursday => 'thu',
-    Friday   => 'fri',
-    Saturday => 'sat',
-    Sunday   => 'sun',
-  );
-}
-
-sub compute_time ($s) {
-  my @words = split /[ ,]/, $s;
-
-  return "" . $days{ $words[0] } . "/" . $words[1];
-}
-
